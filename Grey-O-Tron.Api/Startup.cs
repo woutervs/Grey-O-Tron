bbcb925280377f5dcd1257dbfe26979fefbb1ba8
Wouter Van Speybroeck <wouter.van.speybroeck@gmail.com>
2019-09-29 19:00:58 +0200

    Cleanup startup method

--- Grey-O-Tron.Api/Startup.cs
+++ Grey-O-Tron.Api/Startup.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq.Expressions;
-using Autofac;
+﻿using Autofac;
 using GreyOTron.Library.Helpers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -21,8 +20,6 @@ namespace GreyOTron.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
-            services.AddOptions();
             services.AddCors();
             services.AddControllers();
         }
@@ -30,24 +27,20 @@ namespace GreyOTron.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseRouting();
-            app.UseCors(builder =>
-            {
-                builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
-            });
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
             app.UseHttpsRedirection();
 
+            app.UseRouting();
+
+            app.UseCors(builder =>
+            {
+                builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+            });
+            
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
 
