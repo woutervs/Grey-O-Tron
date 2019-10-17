3c3409245ba5776bd866d8ca341c3c900e6b04c2
Wouter Van Speybroeck <wouter.van.speybroeck@gmail.com>
2019-10-17 09:26:36 +0200

    Bugfix when endpoint seems down but returns bogus error

---
+++ Grey-O-Tron.Library/Exceptions/EndpointRequiresAuthenticationException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace GreyOTron.Library.Exceptions
+{
+    public class EndpointRequiresAuthenticationException : ApiInformationForUserByKeyException
+    {
+        public EndpointRequiresAuthenticationException(string section, string key, string content, Exception innerException) : base(section, key, content, innerException)
+        {
+        }
+    }
+}
